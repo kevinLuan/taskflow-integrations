ce7f967806a4bf848f0053006cc40744cdf3281c
kevin_luan@aliyun.com <>
2025-06-05 10:56:26 +0800

    增加返回对象类型字段解析

--- sdk/src/main/java/cn/feiliu/taskflow/utils/FieldUtils.java
+++ sdk/src/main/java/cn/feiliu/taskflow/utils/FieldUtils.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2024 Taskflow, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.feiliu.taskflow.utils;
 
 import java.lang.reflect.Field;
@@ -41,9 +55,8 @@ public class FieldUtils {
         String fieldName = field.getName();
         String capitalized = capitalize(fieldName);
         // 可能的 Getter 方法名前缀
-        String[] prefixes = field.getType() == boolean.class || field.getType() == Boolean.class
-                ? new String[]{"is", "get"}
-                : new String[]{"get"};
+        String[] prefixes = field.getType() == boolean.class || field.getType() == Boolean.class ? new String[] { "is",
+                "get" } : new String[] { "get" };
 
         for (String prefix : prefixes) {
             try {
@@ -72,15 +85,24 @@ public class FieldUtils {
         Class<?> returnType = getter.getReturnType();
         // 基本类型和其包装类的兼容性检查
         if (fieldType.isPrimitive()) {
-            if (fieldType == boolean.class && returnType == Boolean.class) return true;
-            if (fieldType == int.class && returnType == Integer.class) return true;
-            if (fieldType == double.class && returnType == Double.class) return true;
-            if (fieldType == float.class && returnType == Float.class) return true;
-            if (fieldType == long.class && returnType == Long.class) return true;
-            if (fieldType == short.class && returnType == Short.class) return true;
-            if (fieldType == byte.class && returnType == Byte.class) return true;
-            if (fieldType == char.class && returnType == Character.class) return true;
-            if (fieldType == void.class && returnType == Void.class) return true;
+            if (fieldType == boolean.class && returnType == Boolean.class)
+                return true;
+            if (fieldType == int.class && returnType == Integer.class)
+                return true;
+            if (fieldType == double.class && returnType == Double.class)
+                return true;
+            if (fieldType == float.class && returnType == Float.class)
+                return true;
+            if (fieldType == long.class && returnType == Long.class)
+                return true;
+            if (fieldType == short.class && returnType == Short.class)
+                return true;
+            if (fieldType == byte.class && returnType == Byte.class)
+                return true;
+            if (fieldType == char.class && returnType == Character.class)
+                return true;
+            if (fieldType == void.class && returnType == Void.class)
+                return true;
         }
         return fieldType.isAssignableFrom(returnType);
     }
